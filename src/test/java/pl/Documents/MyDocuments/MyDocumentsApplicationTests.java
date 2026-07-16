7baf17a4999bbeebc689b01427bfbbbd7523e6ff
Mateusz <mati2534paw@gmail.com>
2026-07-16 23:33:14 +0200

    first commit

---
+++ src/test/java/pl/Documents/MyDocuments/MyDocumentsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.Documents.MyDocuments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDocumentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
